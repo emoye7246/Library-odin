3b1ec983accdeb598e59d251736d82b671793804
Elijah Moye <Elijahmoye96@gmail.com>
2024-05-11 23:42:16 -0400

    Almost Done
    Right now im just making it look pretty but this
    project has gone way better than I had originally
    ecpected. I knwo im supposed to recap all that I
    have done but just know this has been a success
    from start to finish. i even went back and studied
    some passed code and all. Things to do though is
    make a read or not read function, finsh stylimg
    specifically the nav, and get creative.

--- java/java.js
+++ java/java.js
@@ -6,35 +6,61 @@ function Book() {
   // the constructor...
 }
 
-function addBookToLibrary(title, author, pages, genre) {
+function addBookToLibrary(title, author, pages, genre, summary) {
   
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.genre = genre;
+    this.summary = summary;
+
 
     this.bookInfo = function() {
-      console.log(this.title, this.author, this.pages, this.genre);
-        myLibrary.push(this.title);
+      console.log(title, author, pages, genre, summary);
+        myLibrary.push(title);
 
+
+        let removeButton = document.createElement("button");
+        removeButton.type = "submit"
+        removeButton.innerHTML = "Remove";
         let para = document.createElement("p");
         let contents = document.getElementById("contents");
         contents.appendChild(para);
-        para.innerHTML = `Book: ${this.title}`;
+        para.innerHTML = `Book: ${title} <br> Author: ${author}`;
+        contents.appendChild(removeButton);
 
-
-    
-    }
+        removeButton.addEventListener("click", (e) => {
 
   
+          e.preventDefault(); 
+          para.remove();
+          removeButton.remove();
+        })
+        
+        
+    }
 }
 
+const kobe = new addBookToLibrary('Muse', 'Kobe Bryant', '200 pages', 'Documentary');
+console.log(kobe.bookInfo());
+
+// UserInput
+
+function UserInput() {
+let inputTitle = document.getElementById("title").value;
+let inputAuthor = document.getElementById("author").value;
+let inputPages = document.getElementById("pages").value; 
+let inputGenre = document.getElementById("genre").value;
+
+let user = new addBookToLibrary(inputTitle, inputAuthor, inputPages, inputGenre);
+console.log(user.bookInfo());
+}
 
 // Buttons
 
 const dialog = document.getElementById("showDialog");
 const display = document.getElementById("displayDialog")
-const closeButton = document.querySelector("close");
+const closeButton = document.querySelector("#close");
 
 dialog.addEventListener("click", () => {
 
--- notes.md
+++ notes.md
@@ -1,6 +1,9 @@
 The objective now 
 
-1. Call the addBookToLibrary Function by using a button or something of the sorts
+1. Call the addBookToLibrary Function by using a button or something of the sorts 
+<!-- Completed -->
 2. Get the User Input and store it in the book.info function within addBookTOlIbrary
+<!-- Completed -->
 3. Display the results in the contents section of the html
+<!-- completed -->
 4. style with css
